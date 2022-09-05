57eb4eb09ebe983fe300a089c62dd37199b1649c
Altruist2692 <altruist2692@gmail.com>
2022-09-05 15:07:05 +0530

    ignore warnings

--- lib/wicked_pdf.rb
+++ lib/wicked_pdf.rb
@@ -72,6 +72,7 @@ class WickedPdf
     command = [@exe_path]
     command << '-q' unless on_windows? # suppress errors on stdout
     command += parse_options(options)
+    command << '--ignore-load-errors'
     command << url
     command << generated_pdf_file.path.to_s
 
